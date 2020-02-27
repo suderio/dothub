d58c2be1b0f073b7df1b1b21368339309c5078b0
Paulo Sudério <paulo.suderio@gmail.com>
2020-02-27 14:23:42 -0300

    Operações básicas

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
+//	"fmt"
 	"log"
 	"os"
 	//	"os/exec"
@@ -12,29 +12,44 @@ var (
 	origin     = flag.String("origin", "master", "Origin (branch, repo or 'local') of the files")
 	branch     = flag.String("branch", "$hostname", "Destination branch")
 	repository = flag.String("repository", "", "Destination repo")
+  file       = flag.String("file", "", "File to add or remove")
 )
 
 func main() {
 	flag.Parse()
 	command := os.Args[1]
 	switch command {
+  // Initializes the repo (init / clone)
 	case "init":
 		initialize()
+  // Updates (pull / push) the repo
 	case "update":
 		update()
-	default:
+  // Calls the merge tool
+  case "solve":
+    solve()
+  // Adds a file to the repo
+  case "add":
+    add(*file)
+  // Removes a file from the repo
+  case "remove":
+    remove(*file)
+  // Copy a file to another branch
+  case "copy":
+    copyToBranch(*file)
+  default:
 		log.Printf("Command %s unknown\n", command)
 	}
 }
 
 func initialize() {
-	fmt.Printf("git clone --bare %s\n", *repository)
-	fmt.Printf("git checkout -b %s\n", *branch)
+	log.Printf("git clone --bare %s\n", *repository)
+	log.Printf("git checkout -b %s\n", *branch)
 }
 
 func update() {
-	fmt.Printf("git pull")
-	fmt.Printf("git push")
+	log.Printf("git pull")
+	log.Printf("git push")
 }
 
 func add(filename string) {
@@ -44,3 +59,12 @@ func add(filename string) {
 func remove(filename string) {
 
 }
+
+func solve() {
+
+}
+
+func copyToBranch(filename string) {
+
+}
+
